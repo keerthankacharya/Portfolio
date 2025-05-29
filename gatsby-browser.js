766b1f332414f914f0ca4b4af583df31aa80885c
KEERTHAN K ACHARYA <123472801+keerthankacharya@users.noreply.github.com>
2025-05-30 01:42:12 +0530

    Search Indexing

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,15 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+
+import React from 'react';
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <meta
+      key="google-site-verification"
+      name="google-site-verification"
+      content="9KaR88072bjKmb71cAkDO2f31IZCTblcQDX200Cv9EI"
+    />,
+  ]);
+};
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,16 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
- // You can delete this file if you're not using it
+import React from 'react';
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <meta
+      key="google-site-verification"
+      name="google-site-verification"
+      content="9KaR88072bjKmb71cAkDO2f31IZCTblcQDX200Cv9EI"
+    />,
+  ]);
+};
+
+// You can delete this file if you're not using it
